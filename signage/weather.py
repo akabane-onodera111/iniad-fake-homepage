32a668d3a1161752674dc78ceaf5be821952c33e
akabane-onodera111 <citydx2023-009@iniad.org>
2024-01-28 13:28:49 +0000

    変数名の修正

--- signage/weather.py
+++ signage/weather.py
@@ -4,15 +4,15 @@ import apis
 
 app = Flask(__name__)
 
-@app.route('/', methods=['GET', 'POST'])
-def weather.html():
+@app.route('weather_tokyo.html', methods=['GET', 'POST'])
+def weather():
   city = '東京都北区赤羽台'
   if request.method == 'POST':
     city = request.form['keyword']
   lat,lng = apis.address_to_latlng(city)
   forecast = apis.get_weather(lat, lng)
 
-  return render_template('weather.html', city=city, lat=lat, lon=lng, forecast=forecast)
+  return render_template('weather_tokyo.html', city=city, lat=lat, lon=lng, forecast=forecast)
 
 if __name__ == '__main__':
   app.run(host='0.0.0.0')
