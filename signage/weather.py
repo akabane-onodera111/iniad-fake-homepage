6e78eb3fac61a3eedc004de0a835b8493a15299e
akabane-onodera111 <citydx2023-009@iniad.org>
2024-01-28 13:13:55 +0000

    Python仮で追加

---
+++ signage/weather.py
@@ -0,0 +1,18 @@
+from flask import Flask, render_template, request
+from urllib import parse
+import apis
+
+app = Flask(__name__)
+
+@app.route('/', methods=['GET', 'POST'])
+def weather.html():
+  city = '東京都北区赤羽台'
+  if request.method == 'POST':
+    city = request.form['keyword']
+  lat,lng = apis.address_to_latlng(city)
+  forecast = apis.get_weather(lat, lng)
+
+  return render_template('weather.html', city=city, lat=lat, lon=lng, forecast=forecast)
+
+if __name__ == '__main__':
+  app.run(host='0.0.0.0')
